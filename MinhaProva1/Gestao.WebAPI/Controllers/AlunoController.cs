6f785da41620c50085355c94efad37158e2e21a1
YuriStefan <yuri_guedes10@hotmail.com>
2025-09-26 20:25:38 -0300

    Camada Controller e Configuração da Injeção de Dependência

---
+++ MinhaProva1/Gestao.WebAPI/Controllers/AlunoController.cs
@@ -0,0 +1,32 @@
+﻿using Gestao.Dominio;
+using Gestao.Servico;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestao.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private readonly IAlunoServico _servico;
+
+        public AlunoController(IAlunoServico servico)
+        {
+            _servico = servico;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            _servico.Adicionar(aluno);
+            return Ok("Aluno cadastrado com sucesso!");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_servico.Listar());
+        }
+    }
+}
--- MinhaProva1/Gestao.WebAPI/Program.cs
+++ MinhaProva1/Gestao.WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Gestao.Repositorio;
+using Gestao.Servico;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EstudanteDbContext>(options =>
+    options.UseInMemoryDatabase("EstudanteDb"));
+
+builder.Services.AddScoped<IAlunoRepositorio, AlunoRepositorio>();
+builder.Services.AddScoped<IAlunoServico, AlunoServico>();
 
 var app = builder.Build();
 
